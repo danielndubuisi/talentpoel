f14e7c7d6cfe9fb14c805a22594879524c451250
cleverboss01 <ndubeansdaniel97@gmail.com>
2023-03-28 18:38:05 +0100

    grid text

--- src/components/GridText.js
+++ src/components/GridText.js
@@ -2,9 +2,20 @@ import "./GridText.css";
 
 const GridText = () => {
   return (
-    <div className="grid-text">
-      <h3>Grid text</h3>
-    </div>
+    <>
+      <div className="grid-text top">
+        <h3>Community managers</h3>
+        <h3>Social media managers</h3>
+        <h3>Sales Managers</h3>
+        <h3>Graphic Designers</h3>
+      </div>
+      <div className="grid-text bottom">
+        <h3>Growth Marketer</h3>
+        <h3>Digital Marketers</h3>
+        <h3>Customer Success Manager</h3>
+        <h3>Content Writers</h3>
+      </div>
+    </>
   );
 };
 
