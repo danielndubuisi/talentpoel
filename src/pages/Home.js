d65d2e6e825865f468ad325c674faa3fc50879cc
goodness065 <goodnesskelechi2017@yahoo.com>
2023-07-30 18:35:27 +0100

    added open graph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+import { config } from "../app.config";
 import "./Home.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -13,20 +15,37 @@ import VettingProcess from "../components/vetting-process/VettingProcess";
 import Faq from "../components/faq/Faq";
 
 const Home = () => {
+  const postTitle = "Talentpoel";
+  const postDescription = "Discover the best non-technical talents in Africa.";
+  const postImageUrl = `${config.IMAGE_BASE_URL}/v1690737482/Header_nzmstx.png`;
+  const postUrl = "https://talentpoel.com/";
   return (
-    <div className="home">
-      <Navbar />
-      <Hero />
-      <TrustedBy />
-      <Cost />
-      <Advantage />
-      <VettingProcess />
-      <BottomSection {...bottomSectionOne} />
-      <HeroSection {...aboutData} />
-      <BottomSection {...bottomSectionTwo} />
-      <CompanySection />
-      <Faq />
-      <Footer />
+    <div>
+      <Helmet>
+        <title>{postTitle}</title>
+        <meta name="description" content={postDescription} />
+        {/* Add Open Graph meta tags */}
+        <meta property="og:title" content={postTitle} />
+        <meta property="og:description" content={postDescription} />
+        <meta property="og:image" content={postImageUrl} />
+        <meta property="og:url" content={postUrl} />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
+      <div className="home">
+        <Navbar />
+        <Hero />
+        <TrustedBy />
+        <Cost />
+        <Advantage />
+        <VettingProcess />
+        <BottomSection {...bottomSectionOne} />
+        <HeroSection {...aboutData} />
+        <BottomSection {...bottomSectionTwo} />
+        <CompanySection />
+        <Faq />
+        <Footer />
+      </div>
     </div>
   );
 };
