9343fa1936283edad859848e323d2c0aa96e860c
goodness065 <goodnesskelechi2017@yahoo.com>
2023-08-01 19:58:53 +0100

    working on the opengraph

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import { Helmet } from "react-helmet";
-import { config } from "../app.config";
+// import { Helmet } from "react-helmet";
+// import { config } from "../app.config";
 import "./Home.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -15,22 +15,21 @@ import VettingProcess from "../components/vetting-process/VettingProcess";
 import Faq from "../components/faq/Faq";
 
 const Home = () => {
-  const postTitle = "Talentpoel";
-  const postDescription = "Discover the best non-technical talents in Africa.";
-  const postImageUrl = `${config.IMAGE_BASE_URL}/v1690737482/Header_nzmstx.png`;
-  const postUrl = "https://talentpoel.com/";
+  // const postTitle = "Talentpoel";
+  // const postDescription = "Discover the best non-technical talents in Africa.";
+  // const postImageUrl = `${config.IMAGE_BASE_URL}/v1690737482/Header_nzmstx.png`;
+  // const postUrl = "https://talentpoel.com/";
   return (
     <div>
-      <Helmet>
+      {/* <Helmet>
         <title>{postTitle}</title>
         <meta name="description" content={postDescription} />
-        {/* Add Open Graph meta tags */}
         <meta property="og:title" content={postTitle} />
         <meta property="og:description" content={postDescription} />
         <meta property="og:image" content={postImageUrl} />
         <meta property="og:url" content={postUrl} />
         <meta property="og:type" content="website" />
-      </Helmet>
+      </Helmet> */}
 
       <div className="home">
         <Navbar />
