8ff12d32b0abc647660534a22d9c6296eba70fec
Anita Ikediashi <anitaikediashi83@gmail.com>
2024-07-19 10:56:36 +0100

    adjust trusted by images width and created review component

--- src/components/CompanySection.js
+++ src/components/CompanySection.js
@@ -1,79 +1,31 @@
 import "./CompanySection.css";
-import vector from "./asset/icons/vector.svg";
-import company from "./asset/icons/company.svg";
-import founders from "./asset/icons/founders.svg";
-import { Button } from "./Button";
-import { Link } from "react-router-dom";
-import { generalRoute } from "../Data/generalRoutes";
+import Slider from "react-slick";
+import review1 from "./asset/reviews/review_1.png"
+import review2 from "./asset/reviews/review_2.png"
+import review3 from "./asset/reviews/review_3.png"
+import review4 from "./asset/reviews/review_4.png"
+import review5 from "./asset/reviews/review_5.png"
+// import vector from "./asset/icons/vector.svg";
+// import company from "./asset/icons/company.svg";
+// import founders from "./asset/icons/founders.svg";
+// import { Button } from "./Button";
+// import { Link } from "react-router-dom";
+// import { generalRoute } from "../Data/generalRoutes";
 
-import image from "./asset/discover/company.webp"
+// import image from "./asset/discover/company.webp"
+
+// const reviews = [
+
+// ]
 
 const CompanySection = () => {
   return (
-    <div className="company-section layout">
-      <div className="img-wrapper">
-        <div>
-          <div className="img-wrapper-img-main">
-            <img
-              src={image}
-              alt="company-img"
-            />
-            <div>
-              <img src={vector} alt="" />
-            </div>
-          </div>
-          <div className="img-wrapper-text">
-            <h3>Talents available</h3>
-            <p>
-              Explore our extensive pool of talent, carefully categorized into
-              five distinct areas: Marketing, Design, Support, Human Management,
-              and Finance.
-              <br /> <span className="find">Find our more below.</span> 
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="company-text-wrapper">
-        <div className="text-wrapper com">
-          <div className="icon-wrapper">
-            <img src={company} alt="company-icon" />
-          </div>
-          <h2 className="company-headline">For companies</h2>
-          <p className="home__hero-subtitle light founders-sub">
-            Start building your team and hire the best non-technical talents
-            in Africa.
-          </p>
-          <Link to={generalRoute.hireATalent}>
-            <Button
-              type="submit"
-              buttonColor="white"
-              buttonStyle="btn--outline"
-              text="Get quote"
-              icon
-              display={true}
-            />
-          </Link>
-        </div>
-        <div className="text-wrapper fou">
-          <div className="icon-wrapper">
-            <img src={founders} alt="company-icon" />
-          </div>
-          <h2 className="company-headline">For founders</h2>
-          <p className="home__hero-subtitle light founders-sub">
-            Start building your team and hire the best non-technical talents
-            in Africa.
-          </p>
-          <Link to={generalRoute.hireATalent}>
-            <Button
-              type="submit"
-              buttonColor="white"
-              buttonStyle="btn--outline"
-              text="Get quote"
-              icon
-              display={true}
-            />
-          </Link>
-        </div>
+    <div className="company-section layout flex_wrapper">
+      <h5 className="company-section_heading">Reviews from our customers</h5>
+      <div className="slider_container">
+        <Slider>
+
+        </Slider>
       </div>
     </div>
   );
